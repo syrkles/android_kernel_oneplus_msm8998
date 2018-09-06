d944fbde7bc4c5a924353b9318a026046ea120a9
feudofjustice <brandanhenderson07@gmail.com>
2018-09-05 23:41:02 -0400

    Build scripts

---
+++ build/anykernel.sh
@@ -0,0 +1,45 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() {
+kernel.string=RedMatter Kernel for OP5/5T by Sayved - Based on Redflare by Maxim Tresk
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus5
+device.name2=OnePlus5T
+device.name3=cheeseburger
+device.name4=dumpling
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## Trim partitions
+fstrim -v /cache
+fstrim -v /data
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+# begin ramdisk changes
+insert_line init.rc "init.redmatter.rc" before "import /init.environ.rc" "import /init.redmatter.rc";
+# end ramdisk changes
+write_boot;
+
+## end install
+
